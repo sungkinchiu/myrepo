2a0767e919894e21aba566db76057aca8bc6432e
sungkinchiu <102130047+sungkinchiu@users.noreply.github.com>
2022-03-24 16:43:00 +0800

    Update BusinessApplication.java

--- business/src/main/java/com/qfedu/business/BusinessApplication.java
+++ business/src/main/java/com/qfedu/business/BusinessApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 // 表示启用 Eureka 客户端
 /**
- * 我提交了了！！！！！！！！！！！
+ * 我提交了了！！！！！！！！！！！sssssss
  */
 @EnableEurekaClient
 public class BusinessApplication {

a3c05d88c920a1ed518cdeecb1706ca8db6be4a3
sungkinchiu <409877921@qq.com>
2022-03-24 18:51:37 +0800

    ok

--- business/src/main/java/com/qfedu/business/BusinessApplication.java
+++ business/src/main/java/com/qfedu/business/BusinessApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-///22229999999
+
 @EnableEurekaClient
 public class BusinessApplication {
 
